46e451d806f0c286210a8681563059c79b83fa56
Xi Pu <xp19@duke.edu>
2018-06-21 17:10:45 +0800

    implement a function to check if a binary tree is a binary search tree

---
+++ src/Algorithm/Tree/validateBST.java
@@ -0,0 +1,16 @@
+package Algorithm.Tree;
+
+public class validateBST {
+    public boolean isValidBST(TreeNode root) {
+        return validBSTHelper(root, null, null);
+    }
+
+    public boolean validBSTHelper(TreeNode root, Integer min, Integer max){
+
+        if(root==null) return true;
+        if((min!=null&&root.data<=min)||(max!=null&&root.data>=max)) return false;
+
+        return validBSTHelper(root.left, min, root.data)&&validBSTHelper(root.right, root.data, max);
+    }
+    
+}
